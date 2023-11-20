f277d27de4c60ddb28562d09c57c5eed3a847900
Esa Leino <esaleino93@gmail.com>
2023-11-20 18:57:54 +0200

    Attempting to change to webpack

--- server/index.ts
+++ server/index.ts
@@ -1,14 +1,21 @@
 import express, { Express, Request, Response } from 'express';
-
+import { Server } from 'socket.io';
 const app: Express = express();
+const http = require('http');
+const server = http.createServer(app);
 const port = process.env.PORT || 3000;
+const io = new Server(server);
 
 app.get('/', (req: Request, res: Response) => {
-	res.send('Express + TypeScript Server');
+	res.sendFile(__dirname + '/public/index.html');
 });
 
-app.listen(port, () => {
+io.on('connection', (socket) => {
+	console.log('a user connected');
+});
+server.listen(port, () => {
 	console.log(process.env.PORT);
+	console.log(new Date());
 	console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
 	console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
 });
